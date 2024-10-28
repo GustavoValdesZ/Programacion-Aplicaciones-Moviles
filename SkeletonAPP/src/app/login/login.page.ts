fad9b86a1f11a6586a1f0b4c0911a33d534cfe08
GustavoValdesZ <gustavovaldes.z@gmail.com>
2024-10-28 20:14:59 -0300

    añadiendo la logica del login en el ts del login

--- SkeletonAPP/src/app/login/login.page.ts
+++ SkeletonAPP/src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  usuario: string = '';
+  password: string = '';
 
-  constructor() { }
+  constructor(private navCTRL: NavController) { }
 
   ngOnInit() {
   }
 
+  login() {
+    this.navCTRL.navigateForward(['/home'], {
+      queryParams: {
+        usuario: this.usuario,
+        password: this.password,
+      }
+    });
+  }
 }
