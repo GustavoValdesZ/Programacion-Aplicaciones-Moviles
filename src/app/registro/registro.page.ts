e00a89a79a34bf1acad65ead8b18dd978235d7e5
GustavoValdesZ <gustavovaldes.z@gmail.com>
2024-11-04 17:04:26 -0300

    se crea registro y sus validadores

---
+++ src/app/registro/registro.page.ts
@@ -0,0 +1,84 @@
+import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.page.html',
+  styleUrls: ['./registro.page.scss'],
+})
+export class RegistroPage {
+  registro = {
+    nombre: '',
+    apellido: '',
+    usuario: '',
+    contrasena: '',
+    nivelEducacion: '',
+    fechaNacimiento: ''
+  };
+
+  constructor(private alertCtrl: AlertController) {}
+
+  async onSubmit() {
+    // Validar el nombre
+    if (!this.registro.nombre) {
+      this.mostrarAlerta('El nombre no puede estar vacío.');
+      return;
+    }
+
+    // Validar el apellido
+    if (!this.registro.apellido) {
+      this.mostrarAlerta('El apellido no puede estar vacío.');
+      return;
+    }
+
+    // Validar el usuario
+    if (!this.validarUsuario(this.registro.usuario)) {
+      this.mostrarAlerta('El usuario debe tener entre 3 y 8 caracteres alfanuméricos.');
+      return;
+    }
+
+    // Validar la contraseña
+    if (!this.registro.contrasena) {
+      this.mostrarAlerta('La contraseña no puede estar vacía.');
+      return;
+    }
+
+    //validar nivel educ
+    if (!this.registro.nivelEducacion) {
+      this.mostrarAlerta('La fecha de nacimiento no puede estar vacía.');
+      return;
+    }
+
+    // Validar la fecha de nacimiento
+    if (!this.registro.fechaNacimiento) {
+      this.mostrarAlerta('La fecha de nacimiento no puede estar vacía.');
+      return;
+    }
+
+    // Si todas las validaciones son exitosas
+    const alert = await this.alertCtrl.create({
+      header: 'Registro Exitoso',
+      message: `Nombre: ${this.registro.nombre}
+                 Apellido: ${this.registro.apellido}
+                 Usuario: ${this.registro.usuario}
+                 Nivel de Educación: ${this.registro.nivelEducacion}
+                 Fecha de Nacimiento: ${this.registro.fechaNacimiento}`,
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
+
+  async mostrarAlerta(mensaje: string) {
+    const alert = await this.alertCtrl.create({
+      header: 'Error',
+      message: mensaje,
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
+
+  private validarUsuario(usuario: string): boolean {
+    const pattern = /^[a-zA-Z0-9]{3,8}$/;
+    return pattern.test(usuario);
+  }
+}
